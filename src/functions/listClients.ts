a5124cd5e2de1827b474477aae3c02e851d0e741
César Tallys <82481963+CesarTHD@users.noreply.github.com>
2024-06-26 23:14:15 -0300

    passando arquivos do next

--- src/functions/listClients.ts
+++ src/functions/listClients.ts
@@ -1,5 +1,5 @@
 async function listClients(cpf_cnpj:string) {
-    const urlApi = `https://api.iugu.com/v1/customers?query=cpf_cnpj%3A${cpf_cnpj}&api_token=${process.env.TOKEN_API}}`
+    const urlApi = `https://api.iugu.com/v1/customers?query=cpf_cnpj%3A${cpf_cnpj}&api_token=38065F43CCBB2D4A4C507782AD80AFA8860B02EBE4C3AFB32ECEDD583464D533`
 
     const options = { method: 'GET', headers: { accept: 'application/json' }, "Access-Control-Allow-Origin": "*" };
 
