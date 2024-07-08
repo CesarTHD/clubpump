2b4642369db2c1d4de31630d7c14effd67ddb1e5
César Tallys <82481963+CesarTHD@users.noreply.github.com>
2024-07-08 19:17:50 -0300

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script type="text/javascript" src="https://js.iugu.com/v2"></script>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
